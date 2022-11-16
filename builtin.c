668d368c214ccb9fb803fc4598cb2ae156da937c
Halabitimoty <halabitimoty@yahoo.com>
2022-11-16 15:05:08 -0800

    bug fixing...

--- builtin.c
+++ builtin.c
@@ -2,8 +2,8 @@
 
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
-int shellby_cd(char **args, char __attribute__((__unused__)) **front);
-int shellby_help(char **args, char __attribute__((__unused__)) **front);
+int shellby_cd(char **args, char __attribute__((__unused__))**front);
+int shellby_help(char **args, char __attribute__((__unused__))**front);
 
 /**
  * get_builtin - Matches a command with a corresponding
@@ -87,7 +87,7 @@ int shellby_exit(char **args, char **front)
  *         If an error occurs - -1.
  *         Otherwise - 0.
  */
-int shellby_cd(char **args, char __attribute__((__unused__)) **front)
+int shellby_cd(char **args, char __attribute__((__unused__))**front)
 {
 	char **dir_info, *new_line = "\n";
 	char *oldpwd = NULL, *pwd = NULL;
@@ -115,7 +115,8 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 		}
 		else
 		{
-			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode) && 
+			if (stat(args[0], &dir) == 0 &&
+					S_ISDIR(dir.st_mode) &&
 					((dir.st_mode & S_IXUSR) != 0))
 				chdir(args[0]);
 			else
